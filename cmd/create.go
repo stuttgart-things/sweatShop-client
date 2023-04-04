a3ab1c46fcd67109f12e4cd8250ef83861a97105
Patrick Hermann <patrick.hermann@sva.de>
2023-04-04 15:57:33 +0000

    added revision runs

--- cmd/create.go
+++ cmd/create.go
@@ -4,8 +4,6 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
-	"os"
 	"strings"
 
 	"github.com/stuttgart-things/yacht-application-client/internal"
@@ -13,34 +11,40 @@ import (
 	"github.com/spf13/cobra"
 )
 
+//var (
+// address = "localhost:50051"
+// address = "yas.dev.sthings.tiab.ssc.sva.de:443"
+// address = "sleeveless-dev.labul.sva.de:8011"
+//)
+
 // createCmd represents the create command
 var createCmd = &cobra.Command{
 	Use:   "create",
 	Short: "create command",
 	Long:  `create command`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("create called")
+
+		// input: get flags
+		endpoint, _ := cmd.LocalFlags().GetString("endpoint")
+		port, _ := cmd.LocalFlags().GetString("port")
+
+		// set adress
+		address := endpoint + ":" + port
+
+		if strings.Contains(address, "localhost") {
+			internal.ConnectInsecure(address)
+		} else {
+			internal.ConnectSecure(address)
+		}
+
 	},
 }
 
-var (
-	//address = "localhost:50051"
-	address = "yas.dev.sthings.tiab.ssc.sva.de:443"
-	//address = "sleeveless-dev.labul.sva.de:8011"
-)
-
 func init() {
 	rootCmd.AddCommand(createCmd)
-
-	// Check env vor given server port
-	if os.Getenv("YAS_URL") != "" {
-		address = os.Getenv("YAS_URL")
-	}
-
-	if strings.Contains(address, "localhost") {
-		internal.ConnectInsecure(address)
-	} else {
-		internal.ConnectSecure(address)
-	}
-
+	createCmd.Flags().String("endpoint", "", "grpc fqdn/ip of yas-endpoint")
+	createCmd.Flags().String("port", "443", "grpc port of yas-endpoint")
+	createCmd.Flags().String("file", "", "file containing revisionRuns")
+	createCmd.MarkFlagRequired("endpoint")
+	createCmd.MarkFlagRequired("file")
 }
